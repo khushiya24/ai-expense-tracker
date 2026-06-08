2e20d128cd66706a27af181bceb9e2d525e68935
Khushiya <khushiyabhoge@gmail.com>
2026-06-08 11:55:50 +0530

    Production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-expense-tracker-rb4b.onrender.com/api",
 });
 
 export default API;
